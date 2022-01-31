7e65578992b748e50034668a4a7e6050874d5507
man <85434597+N3rmis@users.noreply.github.com>
2022-01-31 13:36:40 +0500

    yes
    yes

---
+++ invincibility-hack-1.1/src/main/java/net/invincibilityhack/MainClient.java
@@ -0,0 +1,9 @@
+package net.invincibilityhack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
